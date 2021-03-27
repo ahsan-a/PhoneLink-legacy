e5004983040ff37907640c5eae5389331388213f
Ahsan Ahmed <50714675+TheTechnoGamer01@users.noreply.github.com>
2021-03-27 14:32:58 +0000

    added sending files and better settings interface

--- Phonelink/Phonelink/Form1.Designer.cs
+++ Phonelink/Phonelink/Form1.Designer.cs
@@ -33,10 +33,23 @@ namespace Phonelink
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.trayMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.OpenSettings = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.contextUpdateCheck = new System.Windows.Forms.ToolStripMenuItem();
             this.contextEnablePassword = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.contextMenuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.title = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SubmitNewSettings = new System.Windows.Forms.Button();
+            this.EditSaveFileLocation = new System.Windows.Forms.Button();
+            this.EnablePasswordBox = new System.Windows.Forms.CheckBox();
+            this.UpdateCheckBox = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PortInput = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.PasswordInput = new System.Windows.Forms.TextBox();
+            this.currentFilelocation = new System.Windows.Forms.Label();
             this.trayMenu.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,17 +59,32 @@ namespace Phonelink
             this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIcon.Icon")));
             this.trayIcon.Text = "PhoneLink";
             this.trayIcon.Visible = true;
+            this.trayIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.trayIcon_MouseClick);
             // 
             // trayMenu
             // 
             this.trayMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenSettings,
+            this.toolStripSeparator2,
             this.contextUpdateCheck,
             this.contextEnablePassword,
             this.toolStripSeparator1,
             this.contextMenuExit});
             this.trayMenu.Name = "contextMenuStrip1";
             this.trayMenu.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
-            this.trayMenu.Size = new System.Drawing.Size(200, 98);
+            this.trayMenu.Size = new System.Drawing.Size(200, 104);
+            // 
+            // OpenSettings
+            // 
+            this.OpenSettings.Name = "OpenSettings";
+            this.OpenSettings.Size = new System.Drawing.Size(199, 22);
+            this.OpenSettings.Text = "Open Settings";
+            this.OpenSettings.Click += new System.EventHandler(this.OpenSettings_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(196, 6);
             // 
             // contextUpdateCheck
             // 
@@ -86,19 +114,144 @@ namespace Phonelink
             this.contextMenuExit.Text = "Exit";
             this.contextMenuExit.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
+            // title
+            // 
+            this.title.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.title.Font = new System.Drawing.Font("Segoe UI Semibold", 16F, System.Drawing.FontStyle.Bold);
+            this.title.Location = new System.Drawing.Point(0, 0);
+            this.title.Name = "title";
+            this.title.Padding = new System.Windows.Forms.Padding(0, 7, 0, 0);
+            this.title.Size = new System.Drawing.Size(432, 262);
+            this.title.TabIndex = 6;
+            this.title.Text = "PhoneLink Settings";
+            this.title.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(0, 244);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(432, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Some changes will be applied on app restart. Sorry about this UI.";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // SubmitNewSettings
+            // 
+            this.SubmitNewSettings.Location = new System.Drawing.Point(181, 214);
+            this.SubmitNewSettings.Name = "SubmitNewSettings";
+            this.SubmitNewSettings.Size = new System.Drawing.Size(75, 23);
+            this.SubmitNewSettings.TabIndex = 5;
+            this.SubmitNewSettings.Text = "Submit";
+            this.SubmitNewSettings.UseVisualStyleBackColor = true;
+            this.SubmitNewSettings.Click += new System.EventHandler(this.SubmitNewSettings_Click);
+            // 
+            // EditSaveFileLocation
+            // 
+            this.EditSaveFileLocation.Location = new System.Drawing.Point(148, 166);
+            this.EditSaveFileLocation.Name = "EditSaveFileLocation";
+            this.EditSaveFileLocation.Size = new System.Drawing.Size(141, 23);
+            this.EditSaveFileLocation.TabIndex = 4;
+            this.EditSaveFileLocation.Text = "Edit Save File Location";
+            this.EditSaveFileLocation.UseVisualStyleBackColor = true;
+            this.EditSaveFileLocation.Click += new System.EventHandler(this.EditSaveFileLocation_Click);
+            // 
+            // EnablePasswordBox
+            // 
+            this.EnablePasswordBox.AutoSize = true;
+            this.EnablePasswordBox.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EnablePasswordBox.Location = new System.Drawing.Point(156, 50);
+            this.EnablePasswordBox.Name = "EnablePasswordBox";
+            this.EnablePasswordBox.Size = new System.Drawing.Size(129, 21);
+            this.EnablePasswordBox.TabIndex = 0;
+            this.EnablePasswordBox.Text = "Enable Password";
+            this.EnablePasswordBox.UseVisualStyleBackColor = true;
+            // 
+            // UpdateCheckBox
+            // 
+            this.UpdateCheckBox.AutoSize = true;
+            this.UpdateCheckBox.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UpdateCheckBox.Location = new System.Drawing.Point(152, 75);
+            this.UpdateCheckBox.Name = "UpdateCheckBox";
+            this.UpdateCheckBox.Size = new System.Drawing.Size(138, 21);
+            this.UpdateCheckBox.TabIndex = 1;
+            this.UpdateCheckBox.Text = "Check for Updates";
+            this.UpdateCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(106, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 21);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Edit Port";
+            // 
+            // PortInput
+            // 
+            this.PortInput.Location = new System.Drawing.Point(203, 107);
+            this.PortInput.MaxLength = 6;
+            this.PortInput.Name = "PortInput";
+            this.PortInput.Size = new System.Drawing.Size(141, 20);
+            this.PortInput.TabIndex = 2;
+            this.PortInput.WordWrap = false;
+            this.PortInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(67, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 21);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Edit Password";
+            // 
+            // PasswordInput
+            // 
+            this.PasswordInput.Location = new System.Drawing.Point(203, 133);
+            this.PasswordInput.MaxLength = 6;
+            this.PasswordInput.Name = "PasswordInput";
+            this.PasswordInput.Size = new System.Drawing.Size(141, 20);
+            this.PasswordInput.TabIndex = 3;
+            this.PasswordInput.WordWrap = false;
+            // 
+            // currentFilelocation
+            // 
+            this.currentFilelocation.AutoSize = true;
+            this.currentFilelocation.Location = new System.Drawing.Point(12, 193);
+            this.currentFilelocation.Name = "currentFilelocation";
+            this.currentFilelocation.Size = new System.Drawing.Size(0, 13);
+            this.currentFilelocation.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(432, 209);
-            this.Enabled = false;
+            this.ClientSize = new System.Drawing.Size(432, 262);
+            this.Controls.Add(this.currentFilelocation);
+            this.Controls.Add(this.UpdateCheckBox);
+            this.Controls.Add(this.EnablePasswordBox);
+            this.Controls.Add(this.EditSaveFileLocation);
+            this.Controls.Add(this.SubmitNewSettings);
+            this.Controls.Add(this.PasswordInput);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PortInput);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.title);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Phonelink";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
             this.trayMenu.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -110,6 +263,19 @@ namespace Phonelink
         private System.Windows.Forms.ToolStripMenuItem contextUpdateCheck;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripMenuItem contextEnablePassword;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button SubmitNewSettings;
+        private System.Windows.Forms.Button EditSaveFileLocation;
+        private System.Windows.Forms.CheckBox EnablePasswordBox;
+        private System.Windows.Forms.CheckBox UpdateCheckBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox PortInput;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox PasswordInput;
+        private System.Windows.Forms.ToolStripMenuItem OpenSettings;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.Label currentFilelocation;
     }
 }
 
--- Phonelink/Phonelink/Form1.cs
+++ Phonelink/Phonelink/Form1.cs
@@ -2,7 +2,10 @@
 using Microsoft.Toolkit.Uwp.Notifications;
 using SimpleHttp;
 using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
+using System.Net;
 using System.Windows.Forms;
 
 namespace Phonelink
@@ -14,57 +17,117 @@ namespace Phonelink
             InitializeComponent();
         }
 
-        private void UpdateContextMenu()
-        {
-            contextUpdateCheck.Checked = Convert.ToBoolean(Config.AppSettings.Settings["checkNewRelease"].Value);
-            contextEnablePassword.Checked = Convert.ToBoolean(Config.AppSettings.Settings["passwordEnabled"].Value);
-        }
-
-        private void updateAppConfig()
-        {
-            Config.Save(ConfigurationSaveMode.Modified);
-            ConfigurationManager.RefreshSection("appSettings");
-        }
-
         public static Configuration Config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            UpdateContextMenu();
-            if (Convert.ToBoolean(Config.AppSettings.Settings["passwordEnabled"].Value))
-            {
-                GithubUpdateCheck update = new GithubUpdateCheck("ahsan-a", "PhoneLink");
-                bool isUpdate = update.IsUpdateAvailable(ConfigurationManager.AppSettings.Get("currentVersion"), VersionChange.Minor);
-                if (isUpdate)
-                {
-                    new ToastContentBuilder()
-                        .AddText("Phonelink Update Available")
-                        .AddText("Update to get the latest features. A new release is available on the Github repository. You can disable checking for releases in the PhoneLink menu.")
-                        .AddArgument("action", "openGithub")
-                        .Show();
-                }
-            }
-
+            UpdateMenus();
+            SendUpdateNotif();
             Route.Add("/", (req, res, props) =>
             {
                 res.AsText("server is up, send a link!");
             });
 
             var baseUrl = Convert.ToBoolean(Config.AppSettings.Settings["passwordEnabled"].Value) ? "/" + Config.AppSettings.Settings["password"].Value + "/" : "/";
-            Console.WriteLine(baseUrl);
             Route.Add($"{baseUrl}url/{{url}}", (req, res, props) =>
             {
                 var url = props["url"];
+                if (!(url.StartsWith("http"))) url = $"http://{url}";
                 System.Diagnostics.Process.Start($"{url}");
                 res.AsText($"opened {url} on your computer.");
+                GC.Collect();
             });
+
+            Route.Add($"{baseUrl}file", (req, res, props) =>
+                {
+                    SaveFile(req, res, props);
+                    GC.Collect();
+                },
+                "POST");
 
             HttpServer.ListenAsync(
                 Convert.ToInt32(Config.AppSettings.Settings["port"].Value),
                 System.Threading.CancellationToken.None,
                 Route.OnHttpRequestAsync
             );
+
+            Console.WriteLine("server ready");
         }
+
+        private static void SendUpdateNotif()
+        {
+            if (Convert.ToBoolean(Config.AppSettings.Settings["passwordEnabled"].Value))
+            {
+                var update = new GithubUpdateCheck("ahsan-a", "PhoneLink");
+                var isUpdate = update.IsUpdateAvailable(ConfigurationManager.AppSettings.Get("currentVersion"), VersionChange.Minor);
+                if (isUpdate)
+                {
+                    new ToastContentBuilder()
+                        .AddText("PhoneLink Update Available")
+                        .AddText("Update to get the latest features. A new release is available on the Github repository. You can disable checking for releases in the PhoneLink menu.")
+                        .AddArgument("action", "openGithub")
+                        .Show();
+                }
+            }
+        }
+
+        private void UpdateMenus()
+        {
+            contextUpdateCheck.Checked = Convert.ToBoolean(Config.AppSettings.Settings["checkNewRelease"].Value);
+            contextEnablePassword.Checked = Convert.ToBoolean(Config.AppSettings.Settings["passwordEnabled"].Value);
+            PortInput.Text = Config.AppSettings.Settings["port"].Value;
+            PasswordInput.Text = Config.AppSettings.Settings["password"].Value;
+            EnablePasswordBox.Checked = Convert.ToBoolean(Config.AppSettings.Settings["passwordEnabled"].Value);
+            UpdateCheckBox.Checked = Convert.ToBoolean(Config.AppSettings.Settings["checkNewRelease"].Value);
+            currentFilelocation.Text = Config.AppSettings.Settings["currentSavePath"].Value;
+        }
+
+        private void UpdateAppConfig()
+        {
+            Config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+            UpdateMenus();
+        }
+
+        private void SaveFile(HttpListenerRequest req, HttpListenerResponse res, Dictionary<string, string> args)
+        {
+            var files = req.ParseBody(args);
+            //save files
+            foreach (var f in files.Values)
+            {
+                Directory.CreateDirectory(Config.AppSettings.Settings["currentSavePath"].Value);
+                try
+                {
+                    using (var file = new FileStream(
+                        $"{Config.AppSettings.Settings["currentSavePath"].Value}/{f.FileName}", FileMode.CreateNew,
+                        FileAccess.Write))
+                    {
+                        f.Value.CopyTo(file);
+                        res.AsText($"Copied {Convert.ToString(f.FileName)} to the {Config.AppSettings.Settings["currentSavePath"].Value} folder.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (ex.HResult == -2147024816)
+                    {
+                        var path = getFileName(f.FileName, Config.AppSettings.Settings["currentSavePath"].Value, 0);
+                        using (var file = new FileStream(path, FileMode.CreateNew, FileAccess.Write))
+                        {
+                            f.Value.CopyTo(file);
+                            res.AsText($"Copied {Convert.ToString(f.FileName)} to the {Config.AppSettings.Settings["currentSavePath"].Value} folder.");
+                        }
+                    }
+                }
+            }
+        }
+
+        private string getFileName(string fileName, string path, int i)
+        {
+            while (File.Exists($"{path}/{Path.GetFileNameWithoutExtension(fileName)} ({i}){Path.GetExtension(fileName)}")) i++;
+            return $"{path}/{Path.GetFileNameWithoutExtension(fileName)} ({i}){Path.GetExtension(fileName)}";
+        }
+
+        // Auto generated methods
 
         private void Form1_Shown(object sender, EventArgs e)
         {
@@ -79,13 +142,69 @@ namespace Phonelink
         private void toolStripMenuItem1_CheckedChanged(object sender, EventArgs e)
         {
             Config.AppSettings.Settings["checkNewRelease"].Value = Convert.ToString(contextUpdateCheck.Checked);
-            updateAppConfig();
+            UpdateAppConfig();
         }
 
         private void toolStripMenuItem1_CheckedChanged_1(object sender, EventArgs e)
         {
             Config.AppSettings.Settings["passwordEnabled"].Value = Convert.ToString(contextUpdateCheck.Checked);
-            updateAppConfig();
+            UpdateAppConfig();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+            }
+        }
+
+        private void trayIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Show();
+                WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void SubmitNewSettings_Click(object sender, EventArgs e)
+        {
+            Config.AppSettings.Settings["port"].Value = PortInput.Text;
+            Config.AppSettings.Settings["password"].Value = PasswordInput.Text;
+            Config.AppSettings.Settings["checkNewRelease"].Value = Convert.ToString(UpdateCheckBox.Checked);
+            Config.AppSettings.Settings["passwordEnabled"].Value = Convert.ToString(UpdateCheckBox.Checked);
+            UpdateAppConfig();
+            MessageBox.Show(
+                        "Saved settings. Changing certain settings such as your port and password may need you to restart your app to take effect.");
+            Hide();
+        }
+
+        private void EditSaveFileLocation_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    Config.AppSettings.Settings["currentSavePath"].Value = fbd.SelectedPath;
+                }
+                UpdateAppConfig();
+            }
+        }
+
+        private void OpenSettings_Click(object sender, EventArgs e)
+        {
+            Show();
         }
     }
 }
